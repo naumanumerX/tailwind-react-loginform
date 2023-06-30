936e91f0486c82cd3f8b3de68e6b086fa8e862cd
Nauman <you@example.com>
2023-06-30 21:31:43 +0100

    Add styling to components

--- src/App.js
+++ src/App.js
@@ -6,7 +6,10 @@ function App() {
     <div className="App bg-gray-900 h-screen w-screen relative overflow-hidden flex flex-col justify-center items-center">
       <div className="h-40-r w-40-r bg-gradient-to-r from-yellow-400 to-red-500 rounded-full absolute left-2/3 -top-56 animate-pulse">
 
+      
       </div>
+
+      
       <div className="h-35-r w-35-r bg-gradient-to-r from-red-400 via-pink to-purple-500 rounded-full absolute top-96 -left-20 animate-pulse "></div>
       <div className="container h-96 w-96 bg-white bg-opacity-10 relative z-2 rounded-2xl shadow-5xl border border-r-0 border-b-0 border-opacity-30 backdrop-filter bacdrop-blur-sm">
       <form className="h-full flex flex-col justify-evenly items-center">
